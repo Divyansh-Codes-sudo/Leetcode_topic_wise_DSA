7cacb86a78eb2bb05721bd48e3d7528496a93983
Divyansh Upadhyay <91680650+Divyansh-Codes-sudo@users.noreply.github.com>
2026-07-29 09:35:26 +0530

    GeekHub v2:- Update Solution: Split Array into Minimum Subsets [.cpp] (manual) @ 2026-07-29 04:05

--- Easy/Split Array into Minimum Subsets/split-array-into-minimum-subsets.cpp
+++ Easy/Split Array into Minimum Subsets/split-array-into-minimum-subsets.cpp
@@ -1,16 +1,27 @@
 class Solution {
-  public:
+public:
     int minSubsets(vector<int>& arr) {
-        // code here
-        sort(begin(arr) , end(arr));
+        
+        // Sort the array so that consecutive numbers
+        // appear next to each other.
+        sort(begin(arr), end(arr));
+
+        // At least one subset is required.
         int count = 1;
-        for(int i = 1 ; i < arr.size() ; i++){
-            if (arr[i] - arr[i-1] != 1){
+
+        // Traverse the sorted array.
+        for (int i = 1; i < arr.size(); i++) {
+
+            // If the current element is NOT consecutive
+            // to the previous element (difference != 1),
+            // then it cannot be placed in the same subset.
+            // Start a new subset.
+            if (arr[i] - arr[i - 1] != 1) {
                 count++;
             }
         }
-        
+
+        // Return the minimum number of subsets formed.
         return count;
-        
     }
 };
