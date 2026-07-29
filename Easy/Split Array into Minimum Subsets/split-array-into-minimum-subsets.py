3f793a1d0d6445c0eb04c2e26e58bb5cb95f8cee
Divyansh Upadhyay <91680650+Divyansh-Codes-sudo@users.noreply.github.com>
2026-07-29 09:41:38 +0530

    GeekHub v2:- Update Solution: Split Array into Minimum Subsets [.py] (manual) @ 2026-07-29 04:11

--- Easy/Split Array into Minimum Subsets/split-array-into-minimum-subsets.py
+++ Easy/Split Array into Minimum Subsets/split-array-into-minimum-subsets.py
@@ -1,10 +1,20 @@
 class Solution:
     def minSubsets(self, arr):
-        #code here
+        # Sort the array so that consecutive elements
+        # are placed next to each other.
         arr.sort()
+
+        # At least one subset is always required.
         count = 1
-        for i in range(1 , len(arr)):
-            if arr[i] - arr[i-1] != 1:
-                count+=1
-        
+
+        # Traverse the sorted array.
+        for i in range(1, len(arr)):
+
+            # If the current element is not consecutive
+            # to the previous element (difference != 1),
+            # start a new subset.
+            if arr[i] - arr[i - 1] != 1:
+                count += 1
+
+        # Return the minimum number of subsets.
         return count
