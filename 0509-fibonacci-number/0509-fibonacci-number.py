2981e408c294019c900e907aa1dbfb28003e5676
Divyansh Upadhyay <91680650+Divyansh-Codes-sudo@users.noreply.github.com>
2026-08-20 17:37:04 +0530

    Time: 49 ms (53.5%), Space: 19 MB (89.09%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,9 +4,14 @@ class Solution:
 
         if(n<=1): return n
 
+        dp = [-1 for _ in range(n+1)] 
+        
         def f(n):
         
             if n<=1: return n
-            return f(n-1) + f(n-2) 
+
+            if(dp[n] != -1): return dp[n]
+            dp[n] = f(n-1) + f(n-2) 
+            return dp[n]
 
         return f(n)
