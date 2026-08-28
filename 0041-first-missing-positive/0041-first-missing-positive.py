981f4821443ba6c5d571230cc32b21d4fddc4429
Divyansh Upadhyay <91680650+Divyansh-Codes-sudo@users.noreply.github.com>
2026-08-28 09:49:04 +0530

    Time: 48 ms (59.3%), Space: 39.3 MB (7.45%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -0,0 +1,14 @@
+from collections import Counter
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        mp = Counter(nums)
+
+        for i in range(1 , max(nums)+1):
+            if i not in mp:
+                return i
+        
+        if max(nums) < 0: return 1
+        else : return max(nums)+1
+        
+
+        
